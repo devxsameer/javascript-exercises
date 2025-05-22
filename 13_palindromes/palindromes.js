3eb943a2a9bf9bbafc65709bc97c385cd8d5270d
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2025-05-23 01:25:24 +0530

    docs: :memo: Complete Excercise 12 - 16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const editedStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
 
+  const reversedStr = editedStr.split("").reverse().join("");
+  return reversedStr === editedStr;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (mynum) {
+  num = +mynum;
+  if (num === 0) {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  }
+  let fibSeries = [1, 1];
+  for (let i = 2; i <= num; i++) {
+    pushNum = fibSeries[fibSeries.length - 1] + fibSeries[fibSeries.length - 2];
+    fibSeries.push(pushNum);
+  }
+  return fibSeries[num - 1];
 };
 
 // Do not edit below this line
